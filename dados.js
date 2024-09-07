d90104d9e66ea6787da2e1e56dd15f962d0eebbf
Yago Gonçalves <138723627+yagolima1@users.noreply.github.com>
2024-09-07 01:54:06 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,66 +1,72 @@
-// Criando um array para armazenar os objetos dos personagens
 let personagensVanDerLinde = [
-    {
-      nome: "Arthur Morgan",
-      descricao: "Protagonista do jogo, um fora da lei experiente e leal à gangue Van der Linde.",
-      funcao: "Braço direito de Dutch van der Linde",
-      video: "https://youtu.be/PQ3rPe20mRw?si=plxwRJnQSTN8ldje"
-    },
-    {
-      nome: "Dutch van der Linde",
-      descricao: "Líder carismático e visionário da gangue, que busca um lugar para chamar de lar.",
-      funcao: "Líder da gangue",
-      video: "https://youtu.be/Xr0vZTFB0Pk?si=jdMttw3ao3p23zJ8"
-    },
-    {
-      nome: "John Marston",
-      descricao: "Ex-membro da gangue, busca uma nova vida após os eventos do jogo.",
-      funcao: "Pistoleiro e membro de confiança",
-      video: "https://youtu.be/jIz3Pe3vHLI?si=fSZCNvsWrWxJMPHy"
-    },
-
-    {
-      nome: "Hosea Matthews",
-      descricao: "Um dos membros mais antigos e sábios da gangue, conhecido por sua lealdade e conhecimento sobre a vida no Velho Oeste.",
-      funcao: "Conselheiro e mentor",
-      video: "https://youtu.be/wVmfB5gNt5s?si=wOailsM_cDdpZgPQ"
+  {
+    nome: "Arthur Morgan",
+    descricao: "Protagonista do jogo, um fora da lei experiente e leal à gangue Van der Linde.",
+    funcao: "Braço direito de Dutch van der Linde",
+    video: "https://youtu.be/PQ3rPe20mRw?si=plxwRJnQSTN8ldje",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZMU-LXIS94Jc98SjSBagpB7vCVzFzFctGog&s"
   },
   {
-      nome: "Micah Bell",
-      descricao: "Um jovem ambicioso e traiçoeiro, que se torna uma figura central nos eventos do jogo. Possui uma personalidade manipuladora e cruel.",
-      funcao: "Informante e antagonista",
-      video: "https://youtu.be/YORfNf3okDs?si=_R_txfozIle-g1Kd"
+    nome: "Dutch van der Linde",
+    descricao: "Líder carismático e visionário da gangue, que busca um lugar para chamar de lar.",
+    funcao: "Líder da gangue",
+    video: "https://youtu.be/Xr0vZTFB0Pk?si=jdMttw3ao3p23zJ8",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQq2sBcH4p6R6RSDj6HFMPI4N72hT7qgzIPNA&s"
   },
   {
-      nome: "Charles Smith",
-      descricao: "Um nativo americano que se junta à gangue, conhecido por sua habilidade com arco e flecha e sua natureza calma e gentil.",
-      funcao: "Caçador e companheiro leal",
-      video: "https://youtu.be/0KBC1ovQJ50?si=TyvwUEhkygkGPH5_"
+    nome: "John Marston",
+    descricao: "Ex-membro da gangue, busca uma nova vida após os eventos do jogo.",
+    funcao: "Pistoleiro e membro de confiança",
+    video: "https://youtu.be/jIz3Pe3vHLI?si=fSZCNvsWrWxJMPHy",
+    imagem: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/db56b453-3e5a-4e6c-b64b-1cc7d41016b1/debeqls-fcb66fb7-5232-459f-9822-a2c0277aad55.png/v1/fill/w_796,h_1004/john___red_dead_redemption___png_by_alaanfabioo_debeqls-pre.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTYxMyIsInBhdGgiOiJcL2ZcL2RiNTZiNDUzLTNlNWEtNGU2Yy1iNjRiLTFjYzdkNDEwMTZiMVwvZGViZXFscy1mY2I2NmZiNy01MjMyLTQ1OWYtOTgyMi1hMmMwMjc3YWFkNTUucG5nIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.1w-fU_i1g3XgrQ10pQ8TqZYZssm6l2AiieM8ITkXKAI"
   },
   {
-      nome: "Tio",
-      descricao: "Um homem velho e forte, responsável por cuidar do acampamento e dos animais da gangue.",
-      funcao: "Cuidador do acampamento",
-      video: "https://youtu.be/DXRrWmbxqwA?si=VmNjNlS1BREaV1W5"
+    nome: "Hosea Matthews",
+    descricao: "Um dos membros mais antigos e sábios da gangue, conhecido por sua lealdade e conhecimento sobre a vida no Velho Oeste.",
+    funcao: "Conselheiro e mentor",
+    video: "https://youtu.be/wVmfB5gNt5s?si=wOailsM_cDdpZgPQ",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwrpg1OUicQwc5o7F-oSZqPZGEJ5DHGz8DEQ&s"
   },
   {
-      nome: "Lenny Summers",
-      descricao: "Um jovem idealista e leal à gangue, que admira Dutch van der Linde.",
-      funcao: "Guarda-costas e amigo de Arthur",
-      video: "https://youtu.be/TXUCqHl6YmI?si=OH7c6gJxjN6wZPse"
+    nome: "Micah Bell",
+    descricao: "Um jovem ambicioso e traiçoeiro, que se torna uma figura central nos eventos do jogo. Possui uma personalidade manipuladora e cruel.",
+    funcao: "Informante e antagonista",
+    video: "https://youtu.be/YORfNf3okDs?si=_R_txfozIle-g1Kd",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLGZJ61ipmatB6cE4JBsvPKqpa1lcIGMp3Mw&s"
   },
   {
-      nome: "Sadie Adler",
-      descricao: "Uma mulher vingativa que busca vingança pela morte de sua família, tornando-se uma aliada de Arthur.",
-      funcao: "Vingadora e aliada de Arthur",
-      video: "https://youtu.be/33bJ-x9VGhw?si=lR4YzvcMwqxVDEee"
+    nome: "Charles Smith",
+    descricao: "Um nativo americano que se junta à gangue, conhecido por sua habilidade com arco e flecha e sua natureza calma e gentil.",
+    funcao: "Caçador e companheiro leal",
+    video: "https://youtu.be/0KBC1ovQJ50?si=TyvwUEhkygkGPH5_",
+    imagem: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/db56b453-3e5a-4e6c-b64b-1cc7d41016b1/debeqo5-a5708f7c-86d2-4d7d-8061-676246308fea.png/v1/fill/w_766,h_1043/charles___red_dead_redemption___png_by_alaanfabioo_debeqo5-pre.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTc0MSIsInBhdGgiOiJcL2ZcL2RiNTZiNDUzLTNlNWEtNGU2Yy1iNjRiLTFjYzdkNDEwMTZiMVwvZGViZXFvNS1hNTcwOGY3Yy04NmQyLTRkN2QtODA2MS02NzYyNDYzMDhmZWEucG5nIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.41zR9CqPby_4k3DtRJq0r0uhbSJu6aGlwNV1x4EiXTU"
   },
-
+  {
+    nome: "Tio",
+    descricao: "Um homem velho e forte, responsável por cuidar do acampamento e dos animais da gangue.",
+    funcao: "Cuidador do acampamento",
+    video: "https://youtu.be/DXRrWmbxqwA?si=VmNjNlS1BREaV1W5",
+    imagem: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/db56b453-3e5a-4e6c-b64b-1cc7d41016b1/debequa-8337eb68-b3ff-40a0-acff-3790cd8a56c5.png/v1/fit/w_828,h_916/uncle___red_dead_redemption___png_by_alaanfabioo_debequa-414w-2x.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTQxNiIsInBhdGgiOiJcL2ZcL2RiNTZiNDUzLTNlNWEtNGU2Yy1iNjRiLTFjYzdkNDEwMTZiMVwvZGViZXF1YS04MzM3ZWI2OC1iM2ZmLTQwYTAtYWNmZi0zNzkwY2Q4YTU2YzUucG5nIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.V1-OkFjiMVCTxL4Ilhjy3Ol4OGAu4KwhqM_qxs1QYuY"
+  },
+  {
+    nome: "Lenny Summers",
+    descricao: "Um jovem idealista e leal à gangue, que admira Dutch van der Linde.",
+    funcao: "Guarda-costas e amigo de Arthur",
+    video: "https://youtu.be/TXUCqHl6YmI?si=OH7c6gJxjN6wZPse",
+    imagem: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/db56b453-3e5a-4e6c-b64b-1cc7d41016b1/debeqsx-04ed5f6e-660b-414c-9223-653d3868a736.png/v1/fit/w_539,h_600/lenny___red_dead_redemption___png_by_alaanfabioo_debeqsx-375w-2x.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NjAwIiwicGF0aCI6IlwvZlwvZGI1NmI0NTMtM2U1YS00ZTZjLWI2NGItMWNjN2Q0MTAxNmIxXC9kZWJlcXN4LTA0ZWQ1ZjZlLTY2MGItNDE0Yy05MjIzLTY1M2QzODY4YTczNi5wbmciLCJ3aWR0aCI6Ijw9NTM5In1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.7cI_E12Bo_kZmPj6Hb0l4DNL97L9poKsqT8uZpIgPqc"
+  },
+  {
+    nome: "Sadie Adler",
+    descricao: "Uma mulher vingativa que busca vingança pela morte de sua família, tornando-se uma aliada de Arthur.",
+    funcao: "Vingadora e aliada de Arthur",
+    video: "https://youtu.be/33bJ-x9VGhw?si=lR4YzvcMwqxVDEee",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_2zpvL5VSYqxR9V3FOaGQasdJvhM0ypR06Q&s"
+  },
   {
     nome: "Simon Pearson",
     descricao: "O açougueiro e cozinheiro do acampamento, Pearson. Um degenerado barulhento e alegre que está um tanto em negação sobre a reviravolta que sua vida tomou.",
     funcao: "Cozinheiro da Gangue",
-    video: "https://youtu.be/Mvj3e-ikMA8?si=s-2HCwowsRs0LRUV"
-}
-      
-    ];
+    video: "https://youtu.be/Mvj3e-ikMA8?si=s-2HCwowsRs0LRUV",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBJdDPthKUATa1u3wx6jQRwsj9pKUFBvs01A&s"
+  }
+];
