ee844fad0c30603fd4c3680e853e386b04777598
Yago Gonçalves <138723627+yagolima1@users.noreply.github.com>
2024-09-07 01:02:52 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+function pesquisar() {
+  
+    // Busca os dados dos personagens e exibe no console (para debug)
+    console.log(personagensVanDerLinde);
+  
+    // Obtém a seção onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    if (!campoPesquisa){
+       section.innerHTML = "<p>Nada foi Encontrado !</p>"
+       alert("Você precisa digitar o nome de um personagem")
+        return
+    }
+  
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+    // Inicializa uma string vazia para armazenar os resultados formatados
+    let resultados = "";
+    let nome = "";
+    let descricao = "";
+  
+    // Itera sobre cada personagem da lista
+    for (let dado of personagensVanDerLinde) {
+
+        nome = dado.nome.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+
+        if ( nome.includes(campoPesquisa)|| descricao.includes(campoPesquisa)){
+       
+      // Constrói o HTML para cada personagem, utilizando template literal
+      resultados += `
+        <div class="item-resultado">
+          <h2>
+            <a href="#" target="_blank">${dado.nome}</a>
+          </h2>
+          <p class="descricao-meta">${dado.descricao}</p>
+          <a href=${dado.video} target="_blank">Saiba mais sobre o Personagem</a>
+        </div>
+      `;
+        }
+    }
+  
+    // Insere os resultados formatados na seção HTML
+    section.innerHTML = resultados;
+  }
---
+++ dados.js
@@ -0,0 +1,66 @@
+// Criando um array para armazenar os objetos dos personagens
+let personagensVanDerLinde = [
+    {
+      nome: "Arthur Morgan",
+      descricao: "Protagonista do jogo, um fora da lei experiente e leal à gangue Van der Linde.",
+      funcao: "Braço direito de Dutch van der Linde",
+      video: "https://youtu.be/PQ3rPe20mRw?si=plxwRJnQSTN8ldje"
+    },
+    {
+      nome: "Dutch van der Linde",
+      descricao: "Líder carismático e visionário da gangue, que busca um lugar para chamar de lar.",
+      funcao: "Líder da gangue",
+      video: "https://youtu.be/Xr0vZTFB0Pk?si=jdMttw3ao3p23zJ8"
+    },
+    {
+      nome: "John Marston",
+      descricao: "Ex-membro da gangue, busca uma nova vida após os eventos do jogo.",
+      funcao: "Pistoleiro e membro de confiança",
+      video: "https://youtu.be/jIz3Pe3vHLI?si=fSZCNvsWrWxJMPHy"
+    },
+
+    {
+      nome: "Hosea Matthews",
+      descricao: "Um dos membros mais antigos e sábios da gangue, conhecido por sua lealdade e conhecimento sobre a vida no Velho Oeste.",
+      funcao: "Conselheiro e mentor",
+      video: "https://youtu.be/wVmfB5gNt5s?si=wOailsM_cDdpZgPQ"
+  },
+  {
+      nome: "Micah Bell",
+      descricao: "Um jovem ambicioso e traiçoeiro, que se torna uma figura central nos eventos do jogo. Possui uma personalidade manipuladora e cruel.",
+      funcao: "Informante e antagonista",
+      video: "https://youtu.be/YORfNf3okDs?si=_R_txfozIle-g1Kd"
+  },
+  {
+      nome: "Charles Smith",
+      descricao: "Um nativo americano que se junta à gangue, conhecido por sua habilidade com arco e flecha e sua natureza calma e gentil.",
+      funcao: "Caçador e companheiro leal",
+      video: "https://youtu.be/0KBC1ovQJ50?si=TyvwUEhkygkGPH5_"
+  },
+  {
+      nome: "Tio",
+      descricao: "Um homem velho e forte, responsável por cuidar do acampamento e dos animais da gangue.",
+      funcao: "Cuidador do acampamento",
+      video: "https://youtu.be/DXRrWmbxqwA?si=VmNjNlS1BREaV1W5"
+  },
+  {
+      nome: "Lenny Summers",
+      descricao: "Um jovem idealista e leal à gangue, que admira Dutch van der Linde.",
+      funcao: "Guarda-costas e amigo de Arthur",
+      video: "https://youtu.be/TXUCqHl6YmI?si=OH7c6gJxjN6wZPse"
+  },
+  {
+      nome: "Sadie Adler",
+      descricao: "Uma mulher vingativa que busca vingança pela morte de sua família, tornando-se uma aliada de Arthur.",
+      funcao: "Vingadora e aliada de Arthur",
+      video: "https://youtu.be/33bJ-x9VGhw?si=lR4YzvcMwqxVDEee"
+  },
+
+  {
+    nome: "Simon Pearson",
+    descricao: "O açougueiro e cozinheiro do acampamento, Pearson. Um degenerado barulhento e alegre que está um tanto em negação sobre a reviravolta que sua vida tomou.",
+    funcao: "Cozinheiro da Gangue",
+    video: "https://youtu.be/Mvj3e-ikMA8?si=s-2HCwowsRs0LRUV"
+}
+      
+    ];
